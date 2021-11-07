63709db214256a46a848c4bde564066d6ec01f13
DlaMjz <89750610+DlaMjz@users.noreply.github.com>
2021-11-07 14:35:29 -0300

    Add files via upload

---
+++ js/ejecutarcontacto.js
@@ -0,0 +1,14 @@
+function inclusive()
+    {
+        document.body.style.backgroundColor= "black";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.color = "#90ffe9";
+        document.getElementById("section").style.backgroundColor = "black";
+        document.getElementById("section").style.color = "whitesmoke";
+        document.getElementById("footer").style.backgroundColor = "black";
+    }
+
+
+function copy() {
+    alert("Autora: Daniela Martínez Jiménez. EMAIL: dlamjz22@gmail.com");
+    }
